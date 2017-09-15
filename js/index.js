10d500ecbd98c4b43bb97e037875432bcf7fe78e
jo <jo@surikat.pro>
2017-09-15 20:04:20 +0200

    handle android back hardware button

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import { BackHandler } from "react-native";
+
 import { Root } from "native-base";
-import { addNavigationHelpers } from "react-navigation";
+import { addNavigationHelpers, NavigationActions } from "react-navigation";
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -15,6 +17,22 @@ import { connect } from 'react-redux';
 import AppNavigator from './router/AppNavigator';
 
 class App extends React.Component {
+	
+	componentDidMount() {
+		BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+	}
+	componentWillUnmount() {
+		BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+	}
+	onBackPress = () => {
+		const { dispatch, nav } = this.props;
+		if (nav.index === 0) {
+			return false;
+		}
+		dispatch(NavigationActions.back());
+		return true;
+	};
+	
 	render() {
 		return (
 			<AppNavigator navigation={addNavigationHelpers({
